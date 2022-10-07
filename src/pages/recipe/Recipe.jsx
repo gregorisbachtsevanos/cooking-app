7779f50e21887fb99ffd42550a6b44b66767daeb
GregorisB <gregorbachts@gmail.com>
2022-10-07 17:43:16 +0300

    recipe page

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,8 +1,44 @@
 import "./Recipe.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Recipe = () => {
-	return <div>Recipe</div>;
+	const [recipe, setRecipe] = useState("");
+	const [error, setError] = useState(null);
+	const { id } = useParams();
+
+	useEffect(() => {
+		const getRecipe = async () => {
+			const res = await fetch("http://localhost:3000/recipes/" + id);
+			const data = await res.json();
+
+			if (!res.ok) setError("Not Found");
+
+			setRecipe(data);
+		};
+
+		getRecipe();
+	}, [id]);
+	console.log(recipe);
+	return (
+		<div>
+			<div className="error">{error && <p>{error}</p>}</div>
+			<div className="recipe">
+				{recipe && (
+					<>
+						<h2 className="page-title">{recipe.title}</h2>
+						<p>Takes {recipe.cookingTime} to make</p>
+						<ul>
+							{recipe.ingredients.map((ingredient, index) => (
+								<li key={index}>{ingredient}</li>
+							))}
+						</ul>
+						<p className="method">{recipe.method}</p>
+					</>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Recipe;
