fc55af10092b12e41926eec7182ffe0c24572914
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-11 14:14:39 +0300

    delete recipes

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
+import { projectFirestore } from '../firebase/config';
+
 import "./RecipeList.css";
 
 const RecipeList = ({ recipes }) => {
 	if (recipes.length === 0) {
 		return <div className="error">No recipes to load...</div>;
 	}
+
+	const deleteRecipe = (id) => {
+		projectFirestore.collection('recipes').doc(id).delete()
+		console.log(id)
+	}
+
 	return (
 		<div className="recipe-list">
 			{recipes.map((recipe) => {
@@ -14,6 +22,7 @@ const RecipeList = ({ recipes }) => {
 						<p>{recipe.cookingTime} to make</p>
 						<p>{recipe.method.substring(0, 100)}...</p>
 						<Link to={`recipes/${recipe.id}`}>Cook this</Link>
+						<p className="delete" onClick={() => deleteRecipe(recipe.id)}>x</p>
 					</div>
 				);
 			})}
